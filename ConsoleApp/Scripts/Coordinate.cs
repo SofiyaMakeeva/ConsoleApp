0a7b835a31f5d8509ecbf45deda412ecfcbf778c
SofiyaMakeeva <105315614+SofiyaMakeeva@users.noreply.github.com>
2025-04-04 02:55:06 +0300

    Update Coordinate.cs

--- ConsoleApp/Scripts/Coordinate.cs
+++ ConsoleApp/Scripts/Coordinate.cs
@@ -4,14 +4,16 @@
     {
         public Coordinate()
         {
+            int remainder = 6;
+            
             int minLatitude = -90;
             int maxLatitude = 90;
 
             int minLongitude = -180;
             int maxLongitude = 180;
 
-            Latitude = Math.Round(Utility.GetNumber(minLatitude, maxLatitude), 6);
-            Longitude = Math.Round(Utility.GetNumber(minLongitude, maxLongitude), 6);
+            Latitude = Math.Round(Utility.GetNumber(minLatitude, maxLatitude), remainder);
+            Longitude = Math.Round(Utility.GetNumber(minLongitude, maxLongitude), remainder);
         }
 
         public double Latitude { get; private set; }
